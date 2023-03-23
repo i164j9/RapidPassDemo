711eff85adb9bb6dec323320792ea0d6c400651d
Tom Miller <audiotec69@gmail.com>
2023-03-23 15:47:44 -0500

    more or less did a rewite

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 #define PIN 5
 #define PIN1 4
-// #define PIN2 2
+#define PIN2 2
 
 // Parameter 1 = number of pixels in ring
 // Parameter 2 = Arduino pin number (most are valid)
@@ -15,8 +15,8 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel ring = Adafruit_NeoPixel(21, PIN, NEO_RGB + NEO_KHZ800);
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, PIN1, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(21, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, PIN1, NEO_GRB + NEO_KHZ800);
 // Adafruit_NeoPixel meter = Adafruit_NeoPixel(3, PIN2, NEO_RGBW + NEO_KHZ800);
 
 // 10 sec. wait time between pattern changes
@@ -28,11 +28,11 @@ unsigned long previousMillis = 0;
 uint32_t color = 0x0099FF;
 
 // time between steps in the pattern
-unsigned long patternInterval = 20;
+unsigned long patternInterval = 5;
 // for millis() when last update occoured
 unsigned long lastUpdate = 0;                                    
 // speed for each pattern
-unsigned long intervals[] = {20, 20, 20, 20, 20, 20, 20, 20, 2};
+unsigned long intervals[] = {20, 20, 20, 20, 20, 20, 20, 20, 20};
 
 void colorWipe(uint32_t c);
 void pokeball();
@@ -78,73 +78,62 @@ void changePattern(int pat) {
 
 // Pokeball pattern for rings
 void pokeball() {
-  ring.setPixelColor(0, 0, 0, 0, 255);
-  ring.setPixelColor(1, 0, 0, 0, 255);
-  ring.setPixelColor(2, 0, 0, 0, 255);
-  ring.setPixelColor(3, 0, 0, 0, 255);
-  ring.setPixelColor(4, 0, 255, 0, 0);
-  ring.setPixelColor(5, 0, 255, 0, 0);
-  ring.setPixelColor(6, 0, 255, 0, 0);
-  ring.setPixelColor(7, 0, 0, 0, 255);
-  ring.setPixelColor(8, 0, 0, 0, 255);
-  ring.setPixelColor(9, 0, 0, 0, 255);
-  ring.setPixelColor(10, 0, 0, 0, 255);
-  ring.setPixelColor(11, 0, 0, 0, 255);
-  ring.setPixelColor(12, 0, 0, 0, 255);
-  ring.setPixelColor(13, 0, 255, 0, 0);
-  ring.setPixelColor(14, 0, 255, 0, 0);
-  ring.setPixelColor(15, 0, 255, 0, 0);
-  ring.setPixelColor(16, 0, 255, 0, 0);
-  ring.setPixelColor(17, 0, 255, 0, 0);
-  ring.setPixelColor(18, 0, 255, 0, 0);
+  int RGBWvalues[19][5]={{0, 0, 0, 0, 255},{1, 0, 0, 0, 255},{2, 0, 0, 0, 255},{3, 0, 0, 0, 255},{4, 0, 255, 0, 0},
+  {5, 0, 255, 0, 0},{6, 0, 255, 0, 0},{7, 0, 0, 0, 255},{8, 0, 0, 0, 255},{9, 0, 0, 0, 255},{10, 0, 0, 0, 255},
+  {11, 0, 0, 0, 255},{12, 0, 0, 0, 255},{13, 0, 255, 0, 0},{14, 0, 255, 0, 0},{15, 0, 255, 0, 0},{16, 0, 255, 0, 0},
+  {17, 0, 255, 0, 0},{18, 0, 255, 0, 0}};
+  
+  for (int i = 0; i < 20; i++) {
+    for (int u = 0; u < 6; u++) {
+      ring.setPixelColor(RGBWvalues[i][u],RGBWvalues[i][u+1],RGBWvalues[i][u+2],RGBWvalues[i][u+3]);
+    }
+  }
   ring.show();
 }
 
 // Greatball pattern for rings
 void greatball() {
-  ring.setPixelColor(0, 0, 0, 0, 255);
-  ring.setPixelColor(1, 0, 0, 0, 255);
-  ring.setPixelColor(2, 0, 0, 0, 255);
-  ring.setPixelColor(3, 0, 0, 0, 255);
-  ring.setPixelColor(4, 0, 0, 255, 0);
-  ring.setPixelColor(5, 0, 0, 255, 0);
-  ring.setPixelColor(6, 0, 0, 255, 0);
-  ring.setPixelColor(7, 0, 0, 0, 255);
-  ring.setPixelColor(8, 0, 0, 0, 255);
-  ring.setPixelColor(9, 0, 0, 0, 255);
-  ring.setPixelColor(10, 0, 0, 0, 255);
-  ring.setPixelColor(11, 0, 0, 0, 255);
-  ring.setPixelColor(12, 0, 0, 0, 255);
-  ring.setPixelColor(13, 0, 0, 255, 0);
-  ring.setPixelColor(14, 0, 255, 0, 0);
-  ring.setPixelColor(15, 0, 0, 255, 0);
-  ring.setPixelColor(16, 0, 0, 255, 0);
-  ring.setPixelColor(17, 0, 255, 0, 0);
-  ring.setPixelColor(18, 0, 0, 155, 0);
+  int RGBWvalues[19][5]={{0, 0, 0, 0, 255},{1, 0, 0, 0, 255},{2, 0, 0, 0, 255},{3, 0, 0, 0, 255},{4, 0, 0, 255, 0},
+  {5, 0, 0, 255, 0},{6, 0, 0, 255, 0},{7, 0, 0, 0, 255},{8, 0, 0, 0, 255},{9, 0, 0, 0, 255},{10, 0, 0, 0, 255},
+  {11, 0, 0, 0, 255},{12, 0, 0, 0, 255},{13, 0, 0, 255, 0},{14, 0, 255, 0, 0},{15, 0, 0, 255, 0},{16, 0, 0, 255, 0},
+  {17, 0, 255, 0, 0},{18, 0, 0, 155, 0}};
+
+  for (int i = 0; i < 20; i++) {
+    for (int u = 0; u < 6; u++) {
+      ring.setPixelColor(RGBWvalues[i][u],RGBWvalues[i][u+1],RGBWvalues[i][u+2],RGBWvalues[i][u+3]);
+    }
+  }
   ring.show();
 }
 
 // Ultraball pattern for rings
 void ultraball() {
-  ring.setPixelColor(0, 0, 0, 0, 255);
-  ring.setPixelColor(1, 0, 0, 0, 255);
-  ring.setPixelColor(2, 0, 0, 0, 255);
-  ring.setPixelColor(3, 0, 0, 0, 255);
-  ring.setPixelColor(4, 100, 255, 0, 0);
-  ring.setPixelColor(5, 0, 0, 0, 0);
-  ring.setPixelColor(6, 100, 255, 0, 0);
-  ring.setPixelColor(7, 0, 0, 0, 255);
-  ring.setPixelColor(8, 0, 0, 0, 255);
-  ring.setPixelColor(9, 0, 0, 0, 255);
-  ring.setPixelColor(10, 0, 0, 0, 255);
-  ring.setPixelColor(11, 0, 0, 0, 255);
-  ring.setPixelColor(12, 0, 0, 0, 255);
-  ring.setPixelColor(13, 0, 0, 0, 0);
-  ring.setPixelColor(14, 100, 255, 0, 0);
-  ring.setPixelColor(15, 100, 255, 0, 0);
-  ring.setPixelColor(16, 100, 255, 0, 0);
-  ring.setPixelColor(17, 100, 255, 0, 0);
-  ring.setPixelColor(18, 0, 0, 0, 0);
+  int RGBWvalues[19][5]={
+    {0, 0, 0, 0, 255},
+    {1, 0, 0, 0, 255},
+    {2, 0, 0, 0, 255},
+    {3, 0, 0, 0, 255},
+    {4, 100, 255, 0, 0},
+    {5, 0, 0, 0, 0},
+    {6, 100, 255, 0, 0},
+    {7, 0, 0, 0, 255},
+    {8, 0, 0, 0, 255},
+    {9, 0, 0, 0, 255},
+    {10, 0, 0, 0, 255},
+    {11, 0, 0, 0, 255},
+    {12, 0, 0, 0, 255},
+    {13, 0, 0, 0, 0},
+    {14, 100, 255, 0, 0},
+    {15, 100, 255, 0, 0},
+    {16, 100, 255, 0, 0},
+    {17, 100, 255, 0, 0},
+    {18, 0, 0, 0, 0}};
+
+  for (int i = 0; i < 20; i++) {
+    for (int u = 0; u < 6; u++) {
+      ring.setPixelColor(RGBWvalues[i][u],RGBWvalues[i][u+1],RGBWvalues[i][u+2],RGBWvalues[i][u+3]);
+    }
+  }
   ring.show();
 }
 
@@ -159,14 +148,9 @@ void ultraball() {
 
 // Solid color conduit
 void conduit2() {
-  strip.setPixelColor(0, color);
-  strip.setPixelColor(1, color);
-  strip.setPixelColor(2, color);
-  strip.setPixelColor(3, color);
-  strip.setPixelColor(4, color);
-  strip.setPixelColor(5, color);
-  strip.setPixelColor(6, color);
-  strip.setPixelColor(7, color);
+  for(int i = 0; i < 8; i++){
+    strip.setPixelColor(i, color);
+  }
   strip.show();
 }
 
@@ -274,10 +258,10 @@ uint32_t Wheel(byte WheelPos) {
 
 void setup() {
 // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined(__AVR_ATtiny85__)
-  if (F_CPU == 16000000)
-    clock_prescale_set(clock_div_1);
-#endif
+// #if defined(__AVR_ATtiny85__)
+//   if (F_CPU == 16000000)
+//     clock_prescale_set(clock_div_1);
+// #endif
   // End of trinket special code
 
   // Configure NeoPixel pin for output.
@@ -294,34 +278,38 @@ void setup() {
   // Adjust output brightness. Does not immediately affect what's currently
   // displayed on the LEDs. The next call to show() will refresh the LEDs
   // at this level.
-  strip.setBrightness(15);
+  strip.setBrightness(255);
   // Initialize all pixels to 'off'
   strip.show();
+    
   //  meter.begin();
   //  meter.setBrightness(75);
   //  meter.show(); // Initialize all pixels to 'off'
 }
 
+static int pattern = 0, lastReading;
+
 void loop() {
-  static int pattern = 0, lastReading;
   // conduit1();
   // sets conduit led color to 0x0099FF(light blue)
   conduit2();
  
   if ((millis() - previousMillis) >= interval) {
     previousMillis = millis();
-    // every first second
     pattern++;
     
     // wrap around if too big
     if (pattern > 8) {
       pattern = 0;
     }
+
     // set speed for this pattern
     patternInterval = intervals[pattern];
+    
     // clear out the buffer
     wipe();
   }
+  
   if ((millis() - lastUpdate) > patternInterval)
     changePattern(pattern);
   //  power();
